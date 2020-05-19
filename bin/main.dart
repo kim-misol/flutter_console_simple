121bf32dcb1106edcb1874a423d9fee89d26da85
kim-misol <wonderfulpine@gmail.com>
2020-05-19 09:13:32 +0900

    Moved two files under bin folder

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,15 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:http/http.dart' as http;
+
+Future<String> download(final url) async {
+  final response = await http.get(url);
+  dynamic htmlToParse = '';
+  // If the http request is successful the statusCode will be 200
+  if (response.statusCode == 200) {
+    htmlToParse = response.body;
+  } 
+  return htmlToParse;
+}
+
+void main() async {
+  print(await download('http://www.example.com'));
 }
